ec87cc8cb34016d58e75c4023c9561a744262494
LucasAzLima <lucas.a.lima13@outlook.com>
2018-06-26 19:44:14 -0300

    Projeto servidor

---
+++ QtTcpClientProducer/mainwindow.h
@@ -0,0 +1,34 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+int timming=1, timeid;
+
+public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+   void timerEvent(QTimerEvent *event);
+
+public slots:
+  void tcpConnect();
+  void tcpDisconnect();
+  void startTime();
+  void stopTime();
+  void putData();
+  void Time(int _timming);
+private:
+  Ui::MainWindow *ui;
+  QTcpSocket *socket;
+};
+
+#endif // MAINWINDOW_H
